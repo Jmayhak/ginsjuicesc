199614c3aba313e00f918b30f03092a6624e0d9e
Jonathan Mayhak <jmayhak@gmail.com>
2014-05-19 22:52:36 -0400

    converted site to october cms

---
+++ old/market.php
@@ -0,0 +1,34 @@
+<?php
+
+$menu = 'market';
+
+?>
+
+<?php require('_header.php'); ?>
+
+<div class="row">
+    <div class="span7">
+        <h3>Soda City Market</h3>
+        <ul>
+            <li><strong>Where?</strong> in the 1500 block of Main Street, Columbia, SC 29201</li>
+            <li><strong>When?</strong> Saturdays, 8:00am - 12:00pm</li>
+        </ul>
+
+        <!--<h3>Rosewood Market</h3>
+        <ul>
+            <li><strong>Where?</strong> 2803 Rosewood Drive, Columbia, South Carolina 29205</li>
+            <li><strong>When?</strong> every Thursday from 4-7pm</li>
+        </ul>-->
+
+        <h3>USC Farmers Market</h3>
+        <ul>
+            <li><strong>Where?</strong> Green Street in front of the Russell House</li>
+            <li><strong>When?</strong> 10 a.m. - 2 p.m. on September  10, 17, 24 & October 1, 8, 15 & 22</li>
+        </ul>
+    </div>
+    <div class="span3">
+        <img src="/images/ivory_juice.png" />
+    </div>
+</div>
+
+<?php require('_footer.php'); ?>
---
+++ old/pick-3.php
@@ -0,0 +1,45 @@
+<?php
+
+$menu = 'packages';
+
+?>
+
+<?php require('_header.php'); ?>
+
+    <div class="container">
+        <div class="row">
+        
+        			<div class="span6">
+            			<h4>Gin's Juice Sampler</h4>
+						<div style="text-align:center;">
+							
+							<p><strong>3 juices for 1 day</strong></p>
+							<p><strong>Gin picks 3 juices from her menu!</strong></p>
+							<p><strong>$20 + tax</strong></p>
+						</div>
+												
+						
+                <div>
+                	<p>**juices that contain apple  only contain 2 or 3 slices... not a whole apple, in order to reduce amount of sugar in each juice.</p>
+                </div>
+                
+                <p>Please send an email to <a href="mailto:ginsjuicesc@gmail.com">ginsjuicesc@gmail.com</a> in order to set up a pick up time for your Gin's Juice Sampler package and to pay. Thanks!</p>
+                
+                <div class="form-actions" style="text-align:center;">
+            			<a class="btn btn-primary" href="mailto:ginsjuicesc@gmail.com">Email Gin's Juice</a>
+                </div>
+                                
+            		</div>
+            		
+            		<div class="span6">
+            		<img style="display:block;margin-left:auto;margin-right:auto;" src="../images/DSC_0838.JPG" alt="Short n Sweet Cleanse" />
+            			
+            		</div>
+            		            		
+        </div>
+        
+                        
+    </div>
+
+
+<?php require('_footer.php'); ?>
